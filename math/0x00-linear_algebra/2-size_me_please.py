c9a7a64c0218057e69feec8fc12ca38850bfbd82
oumaymabg <oumaymabou257@gmail.com>
2022-05-28 18:02:12 +0100

    size_me_please

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 def matrix_shape(matrix):
-    """Return the shape of matrix"""
     if not isinstance(matrix, list):
         return []
     return [len(matrix)] + matrix_shape(matrix[0])
