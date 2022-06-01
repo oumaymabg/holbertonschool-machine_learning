4169a289b6073301b0390692f9ef9189ec76d82b
oumaymabg <oumaymabou257@gmail.com>
2022-06-01 09:37:25 +0100

    accross_the_planes

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python3
-"""
-Adds two matrices element-wise
-"""
+"""Peform element-wise addition across multiple dimensions"""
 
 
 def add_matrices2D(mat1, mat2):
-  """
-  add_matrices2D
-  """
+    """Add two same dimension matrices element-wise"""
     if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
         return None
     return [[x + y for x, y in zip(a, b)] for a, b in zip(mat1, mat2)]
